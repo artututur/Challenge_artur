e8b75bdd8244a6ea7d2c0150a1660d8a48b6b381
artututur <artur.ivanov.wsh@gmail.com>
2022-08-21 14:18:43 +0200

    21.08

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -1,18 +1,16 @@
+import time
 from pages.base_page import BasePage
 
 
 class Dashboard(BasePage):
-    main_page_xpath = "// span[contains(@class , 'MuiTypography') and text()= 'Main page']"
-    players_xpath = "//*[@class= 'MuiListItemText-root jss29 jss31']"
-    polski_xpath = "//*[text() = 'Polski']"
-    sign_out_xpath = "//*[text() = 'Sign out']"
-    players_count_xpath = "//*[text()= 'Players count']"
-    matches_count_xpath = "//*[text()= 'Matches count']"
-    reports_count_xpath = "//*[text()= 'Reports count']"
-    events_count_xpath = "//*[text()= 'Events count']"
-    scouts_pane_xpath = "//h2[text()= 'Scouts Panel']"
-    shortcuts_xpath = "//span[text()= 'Add player']"
-    activity_xpath = "//*[@href='/en/players/62f2bce6159aa3d4fa18f4b2/edit']"
+    expected_title = 'Scouts panel'
+    dashboard_url = 'https://scouts-test.futbolkolektyw.pl'
+    element_text = 'Scouts Panel'
+    element_text_xpath = "//*[@id='__next']/div[1]/header/div/h6"
+
+    def title_of_page(self):
+        time.sleep(5)
+        assert self.get_page_title(self.dashboard_url) == self.expected_title
 
 
 
--- pages/login_page.py
+++ pages/login_page.py
@@ -5,7 +5,10 @@ class LoginPage(BasePage):
     login_filed_xpath = "//*[@id='login']"
     password_filed_xpath = "//*[@id= 'password']"
     sign_in_button_xpath = "//*[text()= 'Sign in']"
-
+    login_url = ('https://scouts-test.futbolkolektyw.pl/en')
+    expected_title = "Scouts panel - sign in"
+    title_of_box_xpath = "//*[@id='__next']/form/div/div[1]/h5"
+    title_of_box = "Scouts Panel"
 
     def type_in_email(self, email):
         self.field_send_keys(self.login_filed_xpath, email)
@@ -16,6 +19,7 @@ class LoginPage(BasePage):
     def click_on_the_sign_in_button(self):
         self.click_on_the_element(self.sign_in_button_xpath)
 
-
-
-
+    def title_of_page(self):
+        assert self.get_page_title(self.login_url) == self.expected_title
+    def check_title_of_box(self):
+        self.assert_element_text(self.driver, self.title_of_box_xpath, self.title_of_box)
