1f8d3f0c44b60834d4edbbf162c27c94239cf337
artututur <artur.ivanov.wsh@gmail.com>
2022-08-21 11:16:15 +0200

    1

--- pages/login_page.py
+++ pages/login_page.py
@@ -2,9 +2,20 @@ from pages.base_page import BasePage
 
 
 class LoginPage(BasePage):
-    scouts_Panel_xpath = "//*[text()="Scouts Panel"]"
-    login_xpath = "//*[@id="login-label"]"
-    password_xpath = "//label[text()= "Password"]"
+    login_filed_xpath = "//*[@id='login']"
+    password_filed_xpath = "//*[@id= 'password']"
+    sign_in_button_xpath = "//*[text()= 'Sign in']"
+
 
     def type_in_email(self, email):
-        self.field_send_keys(self.login_field_xpath, email)
+        self.field_send_keys(self.login_filed_xpath, email)
+
+    def type_in_password(self, password):
+        self.field_send_keys(self.password_filed_xpath, password)
+
+    def click_on_the_sign_in_button(self):
+        self.click_on_the_element(self.sign_in_button_xpath)
+
+
+
+
---
+++ test_cases/login_to_the_system.py
@@ -0,0 +1,26 @@
+import os
+import unittest
+from selenium import webdriver
+from pages.login_page import LoginPage
+from utils.settings import DRIVER_PATH, IMPLICITLY_WAIT
+
+
+class TestLoginPage(unittest.TestCase):
+
+    @classmethod
+    def setUp(self):
+        os.chmod(DRIVER_PATH, 755)
+        self.driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+        self.driver.get('https://scouts-test.futbolkolektyw.pl/en')
+        self.driver.fullscreen_window()
+        self.driver.implicitly_wait(IMPLICITLY_WAIT)
+
+    def test_log_in_to_the_system(self):
+        user_login = LoginPage(self.driver)
+        user_login.type_in_email('user03@getnada.com')
+        user_login.type_in_password('Test-1234')
+        user_login.click_on_the_sign_in_button()
+
+    @classmethod
+    def tearDown(self):
+        self.driver.quit()
